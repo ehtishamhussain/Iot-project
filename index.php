0ed921f1cb1ea1be8658adcdbb9f03c4b9a74f4d
Ehtisham <ehtishamhussain59@gmail.com>
2021-07-29 13:57:00 +0500

    First

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://kit.fontawesome.com/2b7720102e.js" crossorigin="anonymous"></script>
+
+    <title>Smart Agriculture</title>
+</head>
+<body>
+    <nav class="nav">
+        <div class="navbarsticky">
+        <ul>
+            <li><a href="#Home">Home</a></li>
+            <li><a href="#moisture-readings">Moisture</a></li>
+            <li><a href="#motor-status">Motor</a></li>
+</ul>
+</div>
+</nav>
+<section>
+    <div class="home-div">
+    <div class="home-div-heading">
+        <h1> Smart Agriculture system </h1>
+        <p> Industry 4.0 </p>
+</div>
+</div> 
+</section>
+
+<section>
+    <div class="secondsecheading">
+        <h1> Functionality </h1>
+        <hr>
+
+</div>
+
+<div class="row">
+    <div id="moisture-readings"class="tiles">
+        <h3> Moisture Readings </h3>
+        <i class="fas fa-thermometer-half fa-4x"></i>
+        <h4>56 %</h4>
+
+</div>
+<div id="motor-status"class="tiles">
+    <h3> Motor </h3>
+    <i class="fas fa-fan fa-4x"></i>
+
+
+    <h4> Status: <h4> <span>On/off</span>
+</div>
+</div>
+
+</section>
+<section>
+    <div class="thirdsecheading">
+        <h1> Graphs Coming soon </h1>
+        <hr>
+    </div>
+</section>
+
+</body>
+</html>
